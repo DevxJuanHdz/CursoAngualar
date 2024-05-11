4b7b63bab4a951e1ef5144a434fa32882619d172
DevxJuanHdz <ja.hernandez.aldaco@ugto.mx>
2024-05-10 23:09:34 -0600

    Event Binding Continuacion

--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -12,9 +12,26 @@ export class PersonasComponent {
   deshabilitar = false;
   //Mensaje que saldra por default
   mensaje = 'No se ha agregado ninguna persona';
+  titulo = '';
+
+
   //Metodo agregarPersona()
   agregarPersona(){
-  //Mensaje que saldra dar click al boton
-  this.mensaje = 'Persona agregada'
+    //Mensaje que saldra dar click al boton
+    this.mensaje = 'Persona agregada'
   }
+
+  //Metodo modificarTitulo()
+  modificarTitulo(event: Event){
+    //Para aceder al valor declaramos lo siguiente-->target hace referencia al elemento "input" y se hace un CAST par acceder al valor del elemento realizado una conversion
+    this.titulo = (<HTMLInputElement>event.target).value
+
+    /*
+      Funcionamiento:
+      1) Se obtiene el valor del input de texto y lo guardamos en la variable titulo
+      2) Desplegamos el nuevo valor con interpolacion y se muestra el valor de la variable titulo
+      3) Asi que conforme se va escribiendo vamos llamando al metodo "modificarTitulo" 
+    */
+  }
+
 }
