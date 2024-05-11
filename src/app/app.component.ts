1be7700968a1431586b142b257051effbe288480
DevxJuanHdz <ja.hernandez.aldaco@ugto.mx>
2024-05-11 01:38:00 -0600

    Two way binging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonaComponent } from './components/persona/persona.component';
 import { PersonasComponent } from './components/personas/personas.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PersonaComponent, PersonasComponent],
+  imports: [RouterOutlet, PersonaComponent, PersonasComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PersonaComponent } from '../persona/persona.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [PersonaComponent],
+  imports: [PersonaComponent, FormsModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.css'
 })
@@ -12,6 +13,7 @@ export class PersonasComponent {
   deshabilitar = false;
   //Mensaje que saldra por default
   mensaje = 'No se ha agregado ninguna persona';
+  //Ejercicio Two Binding ---> Si escribimos dentro de '' se carga por default en el input texto parecido al event binding
   titulo = '';
 
 
@@ -20,18 +22,10 @@ export class PersonasComponent {
     //Mensaje que saldra dar click al boton
     this.mensaje = 'Persona agregada'
   }
-
+  /*
   //Metodo modificarTitulo()
   modificarTitulo(event: Event){
-    //Para aceder al valor declaramos lo siguiente-->target hace referencia al elemento "input" y se hace un CAST par acceder al valor del elemento realizado una conversion
     this.titulo = (<HTMLInputElement>event.target).value
-
-    /*
-      Funcionamiento:
-      1) Se obtiene el valor del input de texto y lo guardamos en la variable titulo
-      2) Desplegamos el nuevo valor con interpolacion y se muestra el valor de la variable titulo
-      3) Asi que conforme se va escribiendo vamos llamando al metodo "modificarTitulo" 
-    */
   }
-
+  */
 }
